6c1fd044f8475a994221bd42e5b44d157acf4265
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-20 22:20:26 +0300

    finish task

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+
+
+            int first, second, third;
+
+            
+            Console.WriteLine("Введите первое число : ");
+            first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число : ");
+            second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число : ");
+            third = int.Parse(Console.ReadLine());
+
+            
+            if (first > second)
+            {
+                if (first > third)
+                {
+                    Console.WriteLine("Максимальное число : " + first);
+                }
+                else
+                {
+                    Console.WriteLine("Максимальное число : " + third);
+                }
+            }
+            else
+            {
+                if (second > third)
+                {
+                    Console.WriteLine("Максимальное число : " + second);
+                }
+                else
+                {
+                    Console.WriteLine("Максимальное число : " + third);
+                }
+            }
+        
